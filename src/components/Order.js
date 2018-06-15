b9d4647e1f45190933439ba038b0696f84dc8ab9
Charpell <charpellumeh@gmail.com>
2018-06-15 09:49:15 +0100

    Displaying the Order State with JSX

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,8 +1,55 @@
 import React from "react";
+import { formatPrice } from "../helpers";
 
 class Order extends React.Component {
+  renderOrder = key => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable = fish.status === "available";
+    if (!isAvailable) {
+      return (
+        <li key={key}>
+          Sorry {fish ? fish.name : "fish"} is no longer available
+        </li>
+      );
+    }
+    return (
+      <li key={key}>
+        {count} lbs {fish.name}
+        {formatPrice(count * fish.price)}
+      </li>
+    );
+  };
+
   render() {
-    return <div className="order">Order!!!</div>;
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === "available";
+      if (isAvailable) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
+    }, 0);
+    
+    // const sampleKey = 'fish1'
+    // console.log('all orders', this.props.order)
+    // console.log('count', this.props.order[sampleKey])
+    // console.log('orderIds', orderIds)
+    // console.log('total', total)
+    // console.log('fish', this.props.fishes[sampleKey])
+    
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+        <ul className="order">{orderIds.map(this.renderOrder)}</ul>
+        <div className="total">
+          Total:
+          <strong>{formatPrice(total)}</strong>
+        </div>
+      </div>
+    );
   }
 }
 
