22f5980d607b1f559e647c002c0deca64f92ed61
Charpell <charpellumeh@gmail.com>
2018-06-15 09:04:39 +0100

    Updating Our Order State

--- src/components/App.js
+++ src/components/App.js
@@ -25,6 +25,15 @@ class App extends React.Component {
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
+  
+  addToOrder = (key) => {
+    // 1. take a copy of state
+    const order = { ...this.state.order };
+    // 2. Either add to the order, or wwupdate the number in our order
+    order[key] = order[key] + 1 || 1;
+    // 3. Call setState to update our state object
+    this.setState({ order });
+  }
 
   render() {
     return (
